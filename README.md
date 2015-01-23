fd08fef4ac0ebfcaec8645b7d9075f9f831d69b2
Samuel Skånberg <samuel.skanberg@pingdom.com>
2015-01-23 11:46:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Foo - super simple theme
+
+My messing around with themes for Drupal 7
+
+# Credits
+
+Thanks to Megan at [http://www.apaddedcell.com/how-create-drupal-7-theme-scratch](http://www.apaddedcell.com/how-create-drupal-7-theme-scratch)
---
+++ page.tpl.php
@@ -0,0 +1,20 @@
+<div id="header">
+    Header
+
+</div>
+
+<div id="wrapper">
+    Wrapper
+    <div id="content">
+        Content
+    </div>
+
+    <div id="sidebar">
+        Sidebar
+    </div>
+
+</div>
+
+<div id="footer">
+    Footer
+</div>
