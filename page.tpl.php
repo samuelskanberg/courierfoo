3ae1a2168294f605889eaf3403984a7bfca32947
Samuel Skånberg <samuel.skanberg@pingdom.com>
2015-01-23 14:49:17 +0100

    More content and basic styling

--- page.tpl.php
+++ page.tpl.php
@@ -1,20 +1,29 @@
 <div id="header">
-    Header
-
+    <h1><?php print $site_name;?></h1>
+    <?php if ($site_slogan): ?>
+        <h2><?php print $site_slogan; ?></h2>
+    <?php endif; ?>
 </div>
 
+<?php if ($main_menu): ?>
+     <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu'))); ?>
+<?php endif; ?>
+
 <div id="wrapper">
-    Wrapper
     <div id="content">
-        Content
+        <?php print render($page['content']); ?>
     </div>
 
-    <div id="sidebar">
-        Sidebar
-    </div>
-
+        <div id="sidebar">
+    <?php if ($page['sidebar_first']): ?>
+        <div id="sidebar">
+            <?php print render($page['sidebar_first']); ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <div id="footer">
-    Footer
+    <?php if($page['footer']): ?>
+        <?php print render($page['footer']); ?>
+    <?php endif; ?>
 </div>
