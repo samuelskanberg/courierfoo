ed47e8155f25b7c52727ab4c0ba5237658ea9908
Samuel Skanberg <samuel.skanberg@gmail.com>
2015-01-27 11:55:01 +0100

    Added page title

--- page.tpl.php
+++ page.tpl.php
@@ -14,6 +14,11 @@
 
 <div id="wrapper">
     <div id="content">
+
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+
         <?php print render($page['content']); ?>
     </div>
 
